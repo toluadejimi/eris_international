11c9aa76736bcfeb3c18d034c9af742df718bc9c
tolu <you@example.com>
2022-12-25 18:51:18 -0800

    PUSH

--- resources/views/academic/grading/includes/form.blade.php
+++ resources/views/academic/grading/includes/form.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
     {!! Form::label('title', 'Title', ['class' => 'col-sm-2 control-label']) !!}
     <div class="col-sm-10">
-        {!! Form::text('title', null, ["placeholder" => "e.g. University Level", "class" => "form-control border-form upper","required"]) !!}
+        {!! Form::text('title', null, ["placeholder" => "e.g. Primary Level", "class" => "form-control border-form upper","required"]) !!}
         @include('includes.form_fields_validation_message', ['name' => 'title'])
     </div>
 </div>
